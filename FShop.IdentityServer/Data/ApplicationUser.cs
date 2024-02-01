9f0a6b9d6b71f58298784849577aacef9980eb29
Gabriel Ferreira <gabrielferreiracunha@gmail.com>
2024-01-31 23:17:42 -0300

    Change:
    Adicionado solução do IdentityServe para autenticação de usuarios

---
+++ FShop.IdentityServer/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FShop.IdentityServer.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = String.Empty;
+        public string LasttName { get; set; } = String.Empty;
+    }
+}
